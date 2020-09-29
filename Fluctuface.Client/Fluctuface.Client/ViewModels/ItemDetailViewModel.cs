76d4aaecf150cc81251d77374630ef31405af9fe
jockf <jockfindlay@gmail.com>
2020-09-29 11:03:29 +0100

    Initial commit
    Intention:
    - Library describing attribute to expose process variable - Fluctuface
    - Service library - Fluctuface.Server
    - Client app (Xamarin/iOS) - Fluctuface.Client
    - FluctufacePlay - Sample application exposing variables to edit

---
+++ Fluctuface.Client/Fluctuface.Client/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Fluctuface.Client.Models;
+
+namespace Fluctuface.Client.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Fluctuface.Server/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Fluctuface.Server
+{
+    public class Class1
+    {
+    }
+}
