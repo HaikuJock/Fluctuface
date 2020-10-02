b158548ef4cf902f71d39ab057bc820393a9920d
jockf <jockfindlay@gmail.com>
2020-10-02 14:08:33 +0100

    Report changes of the fluctuant variable on the server

--- Fluctuface.Server/Program.cs
+++ Fluctuface.Server/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -13,10 +14,25 @@ namespace Fluctuface.Server
     {
         [Fluctuant("First Fluctuant Float", 0f, 1f)]
         public static float FluctuatingFloat = 0.5f;
+        static float PreviousFluctuating = 0f;
 
         public static void Main(string[] args)
         {
+            Task.Factory.StartNew(ReportFloatChanges);
             CreateHostBuilder(args).Build().Run();
+        }
+
+        static void ReportFloatChanges()
+        {
+            while (true)
+            {
+                if (PreviousFluctuating != FluctuatingFloat)
+                {
+                    Console.WriteLine($"Change from: {PreviousFluctuating} to: {FluctuatingFloat}");
+                    PreviousFluctuating = FluctuatingFloat;
+                }
+                Thread.Sleep(1000);
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
