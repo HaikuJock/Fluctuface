40d8771b34621d838efd2a4487e37c259efb105d
jockf <jockfindlay@gmail.com>
2020-10-04 13:00:49 +0100

    Attempting to run the server on a named host

--- Fluctuface.Server/Program.cs
+++ Fluctuface.Server/Program.cs
@@ -14,7 +14,9 @@ namespace Fluctuface.Server
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                        .UseUrls("http://fluctuface:52596")
+                        .UseStartup<Startup>();
                 });
     }
 }
