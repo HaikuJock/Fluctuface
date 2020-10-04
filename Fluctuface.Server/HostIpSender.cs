45bf78d13e3331613b7056db05e3ca0ec5f4136a
jockf <jockfindlay@gmail.com>
2020-10-04 18:51:40 +0100

    Add something to send host ip address over udp

---
+++ Fluctuface.Server/HostIpSender.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluctuface.Server
+{
+    public class HostIpSender
+    {
+        public void Send()
+        {
+            UdpClient client = new UdpClient();
+            IPEndPoint ip = new IPEndPoint(IPAddress.Broadcast, 15000);
+
+            byte[] bytes = Encoding.UTF8.GetBytes("Foo");
+            client.Send(bytes, bytes.Length, ip);
+            client.Close();
+        }
+    }
+}
